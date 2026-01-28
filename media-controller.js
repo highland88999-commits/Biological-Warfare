29957b6bd0b121451969bf604e389d30e6b02cef
highland88999-commits <highland88999@gmail.com>
2026-01-28 06:22:23 -0700

    Update media-controller.js

--- media-controller.js
+++ media-controller.js
@@ -1,37 +1,37 @@
-// media-controller.js
 window.ArtemisMedia = {
-    player: null,
-    
-    init: function() {
-        // Force the YouTube API to load
+    musicPlayer: null,
+
+    // 1. Setup the YT API
+    setup: function() {
         const tag = document.createElement('script');
         tag.src = "https://www.youtube.com/iframe_api";
         document.head.appendChild(tag);
 
-        // Standard YT callback
         window.onYouTubeIframeAPIReady = () => {
-            this.player = new YT.Player('music-host', {
+            this.musicPlayer = new YT.Player('music-host', {
                 videoId: 'qwMz1wgXokk',
                 playerVars: { 'autoplay': 0, 'controls': 0, 'loop': 1, 'playlist': 'qwMz1wgXokk', 'playsinline': 1 },
-                events: { 'onReady': () => console.log("Audio Engine Online") }
             });
         };
     },
 
-    playAll: function(videoId) {
-        // 1. Video
-        const v = document.getElementById(videoId);
+    // 2. The Kickstart - Fired on Landing Page Click
+    ignite: function(videoID) {
+        const v = document.getElementById(videoID);
         v.src = "https://xo6u0xsindhyafqf.public.blob.vercel-storage.com/nexus_render%20%2811%29.webm";
-        v.play().catch(e => console.log("Video manual play required"));
-        v.style.opacity = 1;
+        
+        // Force play
+        v.play().then(() => {
+            v.style.opacity = "1";
+        }).catch(err => console.log("Video Play Error:", err));
 
-        // 2. Music
-        if (this.player && this.player.playVideo) {
-            this.player.unMute();
-            this.player.playVideo();
+        // Unmute and play music
+        if (this.musicPlayer && this.musicPlayer.playVideo) {
+            this.musicPlayer.unMute();
+            this.musicPlayer.playVideo();
         }
     }
 };
 
-// Start the loading process immediately
-ArtemisMedia.init();
+// Initialize immediately on load
+ArtemisMedia.setup();
